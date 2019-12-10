3d59319b72bc303b9468ba75aaaa82052ac5adbd
Aleksandr Podolyanskiy <aleksandr.podolyanskiy@duke.edu>
2019-12-10 11:08:48 -0500

    First commit

---
+++ lib/x_repo.ex
@@ -0,0 +1,18 @@
+defmodule XRepo do
+  @moduledoc """
+  Documentation for XRepo.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> XRepo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
