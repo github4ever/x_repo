663391ced5b8c68ea69fdaa9f006ab6993e42056
Aleksandr Podolyanskiy <aleksandr.podolyanskiy@duke.edu>
2019-12-10 11:36:14 -0500

    Second commit

---
+++ lib/kache.ex
@@ -0,0 +1,31 @@
+defmodule XRepo.Kache do
+use GenServer
+
+def start_link do
+  GenServer.start_link(__MODULE__, %{})
+end
+
+def init(state) do
+  start_ets() |> IO.inspect
+  {:ok, state}
+end
+
+def handle_info(:start_ets, state) do
+  item = start_ets()
+
+  {:noreply, [item | state]}
+end
+
+def start_ets() do
+  :ets.new(:xtable, [
+    :set,
+    :public,
+    :named_table
+  ])
+  insert_ets()
+end
+def insert_ets() do
+  :ets.insert(:xtable, {:hello, "world"})
+end
+
+end
