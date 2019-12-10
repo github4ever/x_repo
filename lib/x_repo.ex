ef3967eda72b8c22314f9dde65204f3c18c4a60c
Aleksandr Podolyanskiy <aleksandr.podolyanskiy@duke.edu>
2019-12-10 11:35:37 -0500

    Second commit

--- lib/x_repo.ex
+++ lib/x_repo.ex
@@ -1,18 +1,22 @@
-defmodule XRepo do
+defmodule XRepo.Application do
   @moduledoc """
   Documentation for XRepo.
   """
+  use Application
 
-  @doc """
-  Hello world.
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
 
-  ## Examples
+    children = [
+      # Define workers and child supervisors to be supervised
+      worker(XRepo.Kache, [])
+    ]
 
-      iex> XRepo.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: XRepo.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
